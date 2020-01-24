03afa99b95e3eeb9654ad05a29d9cf301b2e87d5
suvodeep4119 <suvodeep4119@gmail.com>
2020-01-24 11:41:29 +0530

    ADDED: 4th compute feature type with %

--- arithmeticComputeSort.sh
+++ arithmeticComputeSort.sh
@@ -7,5 +7,7 @@ read -p "Third input:" c
 compute1=`echo "scale=2; $a+$b*$c" | bc` 
 compute2=`echo "scale=2; $a*$b+$c" | bc`
 compute3=`echo "scale=2; $c+$a/$b" | bc`
+compute4=`echo "scale=2; $a%$b+$c" | bc`
+
 
 
