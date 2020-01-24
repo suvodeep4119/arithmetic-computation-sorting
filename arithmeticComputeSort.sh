976a44ca68607cdcf81504074a2df256f1cdc1ce
suvodeep4119 <suvodeep4119@gmail.com>
2020-01-24 12:43:10 +0530

    ADDED:Feature to store compute values as Key-Value

--- arithmeticComputeSort.sh
+++ arithmeticComputeSort.sh
@@ -7,7 +7,11 @@ read -p "Third input:" c
 compute1=`echo "scale=2; $a+$b*$c" | bc` 
 compute2=`echo "scale=2; $a*$b+$c" | bc`
 compute3=`echo "scale=2; $c+$a/$b" | bc`
-compute4=`echo "scale=2; $a%$b+$c" | bc`
+compute4=`echo "scale=2; $(echo $(($a%$b)) )+$c" | bc`
+
+declare -A computes=( ["type1"]="$compute1" ["type2"]="$compute2" ["type3"]="$compute3" ["type4"]="$compute4" )
+${computes[@]}
+
 
 
 
